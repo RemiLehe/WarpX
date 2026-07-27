56f0f3d697782677e466bdc534baf39bbe2f493b
Edoardo Zoni <59625522+EZoni@users.noreply.github.com>
2026-07-27 16:35:34 -0700

    Do not bold Laplacian operator (scalar)
    Co-authored-by: Edoardo Zoni <59625522+EZoni@users.noreply.github.com>

--- Source/FieldSolver/MagnetostaticSolver/MagnetostaticSolver.cpp
+++ Source/FieldSolver/MagnetostaticSolver/MagnetostaticSolver.cpp
@@ -140,7 +140,7 @@ WarpX::AddMagnetostaticFieldLabFrame()
 
     More specifically, this solves the equation
     \f[
-        \boldsymbol{\nabla}^2 r \boldsymbol{A} = - r \mu_0 \boldsymbol{J}
+        \nabla^2 r \boldsymbol{A} = - r \mu_0 \boldsymbol{J}
  \f]
 
    \param[in] curr The current density
