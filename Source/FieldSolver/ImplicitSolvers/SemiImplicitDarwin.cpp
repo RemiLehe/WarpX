1a3be3141dbdf82d7c46ad907555d050ea1abc78
Roelof Groenewald <rgroenewald@realtafusion.com>
2026-09-02 16:59:03 -0500

    second attempt to fix clang-tidy issue
    Signed-off-by: Roelof Groenewald <rgroenewald@realtafusion.com>

--- Source/FieldSolver/ImplicitSolvers/SemiImplicitDarwin.cpp
+++ Source/FieldSolver/ImplicitSolvers/SemiImplicitDarwin.cpp
@@ -613,9 +613,9 @@ void SemiImplicitDarwin::ScaledMassMatrixCC ( amrex::MultiFab& a_chi_cc ) const
     for (int d = 0; d < 3; ++d) {
         const int nc = Sdiag[d]->nComp();
         const amrex::IntVect et = Sdiag[d]->ixType().toIntVect();
-        int e0, e1, e2;
-        e0 = e1 = e2 = 0;
-        AMREX_D_TERM(e0 = et[0];, e1 = et[1];, e2 = et[2];)
+        const int e0 = et[0];
+        const int e1 = (AMREX_SPACEDIM >= 2) ? et[1] : 0;
+        const int e2 = (AMREX_SPACEDIM >= 3) ? et[2] : 0;
         // Each staggered point contributes with equal weight to the average
         // onto the cell center (2 points per nodal dimension of the block).
         const amrex::Real wt =
